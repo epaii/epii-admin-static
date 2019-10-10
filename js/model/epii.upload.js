bf24c0090b5cdf6446e984856421f2dde352214a
zhangshaomin <1208119084@qq.com>
2019-10-10 16:45:00 +0800

    123

--- js/model/epii.upload.js
+++ js/model/epii.upload.js
@@ -90,13 +90,13 @@ define(["plupload", "jquery"], function (Plupload, $) {
                     switch(type[1]){
                         case "zip":
                             //执行代码
-                            $("#"+imgs_ul_id).append("<div class='img_bg' style='margin: 30px 10px;transition: all 0.6s;margin-top: 30px;position: relative;float: left;'><img class='tu_img' src='https://www.easyicon.net/api/resizeApi.php?id=1081640&size=128' style='padding:26px ;border-radius: 10px;width:200px !important; border:0.5px solid #dcdcdc;"+img_style+"'><img style='position: absolute;width: 28px;top:0px;right: 0px;' src='https://www.easyicon.net/api/resizeApi.php?id=1081640&size=128'></div>");
+                            $("#"+imgs_ul_id).append("<div class='img_bg' style='margin: 30px 10px;transition: all 0.6s;margin-top: 30px;position: relative;float: left;'><img class='tu_img' src='https://www.easyicon.net/api/resizeApi.php?id=1169012&size=128' style='padding:26px ;border-radius: 10px;width:200px !important; border:0.5px solid #dcdcdc;"+img_style+"'><img style='position: absolute;width: 28px;top:0px;right: 0px;' src='https://www.easyicon.net/api/resizeApi.php?id=1081640&size=128'></div>");
 
                             $("#"+imgs_ul_id).show();
                             break;
                         case "pdf":
                             //执行代码
-                            $("#"+imgs_ul_id).append("<div class='img_bg' style='margin: 30px 10px;transition: all 0.6s;margin-top: 30px;position: relative;float: left;'><img class='tu_img' src='https://www.easyicon.net/api/resizeApi.php?id=1081640&size=129' style='padding:26px ;border-radius: 10px;width:200px !important; border:0.5px solid #dcdcdc;"+img_style+"'><img style='position: absolute;width: 28px;top:0px;right: 0px;' src='https://www.easyicon.net/api/resizeApi.php?id=1081640&size=128'></div>");
+                            $("#"+imgs_ul_id).append("<div class='img_bg' style='margin: 30px 10px;transition: all 0.6s;margin-top: 30px;position: relative;float: left;'><img class='tu_img' src='https://www.easyicon.net/api/resizeApi.php?id=1129287&size=128' style='padding:26px ;border-radius: 10px;width:200px !important; border:0.5px solid #dcdcdc;"+img_style+"'><img style='position: absolute;width: 28px;top:0px;right: 0px;' src='https://www.easyicon.net/api/resizeApi.php?id=1081640&size=128'></div>");
 
                             $("#"+imgs_ul_id).show();
                             break;
