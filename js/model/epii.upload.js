32edb4d54a85f08add275dba16e652ac55809a14
zhangshaomin <1208119084@qq.com>
2019-10-10 14:33:01 +0800

    ceshi

--- js/model/epii.upload.js
+++ js/model/epii.upload.js
@@ -163,7 +163,7 @@ define(["plupload", "jquery"], function (Plupload, $) {
                         img.show();
 
                     }else if (imgs_ul_id &&  $(button).data("multiple")) {
-                        $("#"+imgs_ul_id).append("<div style='position: relative;sfloat: left;'><img src=\""+response.url+"\" style='padding:26px ;width:200px !important; border:0.5px solid #dcdcdc;margin: 24px 3px; "+img_style+"'><img style='position: absolute;width: 23px;top:26px;right: 4px;' src='https://www.easyicon.net/api/resizeApi.php?id=1140620&size=128'></div>");
+                        $("#"+imgs_ul_id).append("<div style='position: relative;float: left;'><img src=\""+response.url+"\" style='padding:26px ;width:200px !important; border:0.5px solid #dcdcdc;margin: 24px 3px; "+img_style+"'><img style='position: absolute;width: 23px;top:26px;right: 4px;' src='https://www.easyicon.net/api/resizeApi.php?id=1140620&size=128'></div>");
 
                         $("#"+imgs_ul_id).show();
 
@@ -178,7 +178,7 @@ define(["plupload", "jquery"], function (Plupload, $) {
 
                 },
                 UploadComplete: function (up, files) {
-                    alert("UploadComplete")
+                    // alert("UploadComplete")
                     var button = up.settings.button;
                     $(button).prop("disabled", false).html($(button).data("bakup-html"));
                     var onDomUploadSuccess = $(button).data("upload-complete");
